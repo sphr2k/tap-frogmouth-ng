508feef7d9248b900297e93c4705de98933054b2
Jan Werner <mail@janwerner.de>
2026-02-21 15:28:43 +0100

    Use pygments wheel to fix Python 3.14 build

--- Formula/frogmouth-ng.rb
+++ Formula/frogmouth-ng.rb
@@ -10,9 +10,9 @@ class FrogmouthNg < Formula
   depends_on "python3"
   conflicts_with "frogmouth", because: "both install a `frogmouth` executable"
 
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
-    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c7/21/705964c7812476f378728bdf590ca4b771ec72385c533964653c68e86bdc/pygments-2.19.2-py3-none-any.whl"
+    sha256 "86540386c03d588bb81d44bc3928634ff26449851e99741617ecb9037ee5ec0b"
   end
 
   resource "anyio" do
